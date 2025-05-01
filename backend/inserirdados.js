27a24c2ff11457d3c4f026141e32664810c77d9d
victorroch4 <victor11030924@gmail.com>
2025-04-30 22:45:55 -0300

    integracao com novo banco

--- backend/inserirdados.js
+++ backend/inserirdados.js
@@ -2,7 +2,7 @@ const client = require('./db');  // Agora você importa diretamente a instância
 
 // Função para inserir dados de BPM no banco
 const insertData = (macAddress, bpm) => {
-  const query = 'INSERT INTO batimentos(mac_address, bpm) VALUES($1, $2)';
+  const query = 'INSERT INTO measurements(mac_address, bpm) VALUES($1, $2)';
   const values = [macAddress, bpm];
 
   // Inserção no banco de dados
