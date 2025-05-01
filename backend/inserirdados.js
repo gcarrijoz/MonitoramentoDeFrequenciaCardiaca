fadb6a9e267fbe7ddabd9cca6996789fc76c6e80
victorroch4 <victor11030924@gmail.com>
2025-04-30 21:45:15 -0300

    conexao backend com servidor e insercao no banco

---
+++ backend/inserirdados.js
@@ -0,0 +1,14 @@
+const client = require('./db');  // Agora você importa diretamente a instância do cliente
+
+// Função para inserir dados de BPM no banco
+const insertData = (macAddress, bpm) => {
+  const query = 'INSERT INTO batimentos(mac_address, bpm) VALUES($1, $2)';
+  const values = [macAddress, bpm];
+
+  // Inserção no banco de dados
+  client.query(query, values)
+    .then(res => console.log('Dados inseridos:', res))
+    .catch(err => console.error('Erro ao inserir dados no banco', err));
+};
+
+module.exports = insertData;
